3a2c0904a702257fb2bc9d1ee744c9637a3c2b99
Eudson Duraes Silva <eudson.duraes@gmail.com>
2019-06-08 21:03:52 -0300

    Tuplas

--- Exercicios-Python/Basico/exercicio77.py
+++ Exercicios-Python/Basico/exercicio77.py
@@ -1,18 +1,19 @@
 # Crie um programa que tenha uma tupla com várias palavras(não usar acentos).
 # Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais.
-
-teste = (
-    'Eudson', 'Donateu'
-)
-vogais = (
-    'A', 'E', 'I', 'O', 'U',
-    'a', 'e', 'i', 'o', 'u',
-)
+#-----------------------------------------------------------------------
+# ----------------------------------------------------------------------
 # Primeiramente o primeiro FOR e executado de acordo do tamanho do array
 # "Teste" (no caso 2), depois o segundo FOR executado de acordo com o
 # tamanho da palavra infromado, depois e feito a verificação de cada letra
 # da palavra usando as variáveis I e J (I para cada palavra e J para cada
 # letra)
+teste = (
+    'Eudson', 'Donateu','Murata','Cachorro'
+)
+vogais = (
+    'A', 'E', 'I', 'O', 'U',
+    'a', 'e', 'i', 'o', 'u',
+)
 for i in range(0, len(teste)):
     print(f"\nNa palavra {teste[i]} temos as vogais: ", end='')
     for t in range(0, len(teste[i])):
